b610e5fdbe72e364f3e14300321bbce0ff61a0fb
이지운 <ijiun@ijiun-ui-MacBookPro.local>
2026-03-26 19:19:24 +0900

    fix gmail smtp

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -17,12 +17,14 @@ export async function POST(req: Request) {
     const instagram = formData.get("instagram") as string
 
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com", 
+      port:465, 
+      secure: true, 
       auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+       user: process.env.EMAIL_USER,
+       pass: process.env.EMAIL_PASS,
       },
-    })
+     })
 
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
